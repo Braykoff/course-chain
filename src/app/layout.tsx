0b5f4ac0cbf1e879843df65ffdb9cfe97ef62404
Brendan Raykoff <99614905+Braykoff@users.noreply.github.com>
2026-08-27 00:17:52 -0700

    Removed starter files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
 import "@/lib/fontawesome";
 import { TopBar } from "@/components/TopBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -23,7 +18,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} h-full antialiased`}
     >
       <body className="flex min-h-full flex-col bg-white text-gray-900">
         {/* Global top bar */}
